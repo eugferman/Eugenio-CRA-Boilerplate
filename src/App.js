8458f1615905f33af34dcbdb7c3b3b3ff2f46e07
eugferman <eugferman@gmail.com>
2019-11-23 19:43:43 +0100

    router layout setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+
 
 function App() {
   return (
-    <div>Eugenio React boilerplate</div>
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/home" component={Home}><Home /></Route>
+          <Route path="/about" component={About}><About /></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
