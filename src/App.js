c98127e9e862dcec2b1e1817361258df8c153614
eugferman <eugferman@gmail.com>
2019-11-24 20:06:40 +0100

    redux installed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,38 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider, useSelector, useDispatch } from 'react-redux';
 
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 
+function counterReducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case 'INCREMENT_COUNT':
+      return {
+        ...state,
+        count: state.count + 1
+      };
+    case 'DECREMENT_COUNT':
+      return {
+        ...state,
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
+}
+
+
+const rootReducer = combineReducers({
+  counterReducer
+});
+
+const INITIAL_STATE = {};
+
+const store = createStore(rootReducer, INITIAL_STATE);
 
 function App() {
   return (
@@ -20,6 +47,36 @@ function App() {
           <Route path="/about" component={About}><About /></Route>
         </Switch>
       </Router>
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    </>
+  );
+}
+
+function Counter() {
+  const { count } = useSelector((state) => ({
+    ...state.counterReducer
+  }));
+  const dispatch = useDispatch();
+
+  function incrementCount() {
+    dispatch({
+      type: 'INCREMENT_COUNT'
+    });
+  }
+
+  function decrementCount() {
+    dispatch({
+      type: 'DECREMENT_COUNT'
+    });
+  }
+
+  return (
+    <>
+      <h2>Counter: {count}</h2>
+      <button type="button" onClick={incrementCount}>+</button>
+      <button type="button" onClick={decrementCount}>-</button>
     </>
   );
 }
